15a96282a27f8e99136eca874e84d63368f29731
saluumaa <salumahassan02@gmail.com>
2023-02-20 16:43:31 +0300

    Fix eslint errors at datetime.js file

--- modules/datetime.js
+++ modules/datetime.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-unresolved
 import { DateTime } from '../../node_modules/luxon/src/luxon.js';
+
 export default function getDateTime() {
   const todaypar = document.getElementById('date');
   const dt = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
