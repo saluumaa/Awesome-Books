020cfb9c399208e93ba162f39ead52582c0ce1a9
saluumaa <salumahassan02@gmail.com>
2023-02-20 15:45:22 +0300

    import date from luxon

--- modules/datetime.js
+++ modules/datetime.js
@@ -1,6 +1,5 @@
 // eslint-disable-next-line import/no-unresolved
 import { DateTime } from '../../node_modules/luxon/src/luxon.js';
-
 export default function getDateTime() {
   const todaypar = document.getElementById('date');
   const dt = DateTime.now().toLocaleString(DateTime.DATETIME_MED);
