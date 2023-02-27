b3e996cbdddf62d5599f3c79bb6e8102bae43c2e
saluumaa <salumahassan02@gmail.com>
2023-02-27 11:43:50 +0300

    import luxon

--- modules/datetime.js
+++ modules/datetime.js
@@ -1,4 +1,4 @@
-import { DateTime } from '../node_modules/luxon/src/luxon.js';
+import { DateTime } from './luxon/src/luxon.js';
 
 const getDateTime = () => {
   const todaypar = document.getElementById('date');
